da05911060b8dce694faaf8937a088f4d642b6d6
Khelendrameena <168931780+Khelendrameena@users.noreply.github.com>
2024-08-10 21:39:49 +0530

    Update gan.py

--- gan.py
+++ gan.py
@@ -33,7 +33,7 @@ def data_customize(focus=False):
 data_customize()
 
 #Path to your image directory
-def data(path,focus=Flase):
+def data(path,focus=False):
 	image_dir = path
 	dataset = datasets.ImageFolder(root=image_dir, transform=data_customize(focus))
 	return DataLoader(dataset, batch_size=batch_size, shuffle=True)
